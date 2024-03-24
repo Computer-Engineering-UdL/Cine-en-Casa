29ecf5ca57ea35e108b9e7810733d69b31b23c2b
julianogales <julianolo71@gmail.com>
2024-03-24 22:03:56 +0100

    feat: create_billboard

--- CineEnCasa/views.py
+++ CineEnCasa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from CineEnCasa.forms import FilmForm
+from CineEnCasa.forms import FilmForm, BillboardForm
 from CineEnCasa.models import Film
 
 
@@ -25,3 +25,14 @@ def film_detail(request, title):
     film = get_object_or_404(Film, title=title)
     genres = film.genre.all()
     return render(request, 'film_detail.html', {'film': film, 'genres': genres})
+
+
+def create_billboard(request):
+    if request.method == 'POST':
+        form = BillboardForm(request.POST)
+        if form.is_valid():
+            billboard = form.save()
+            return redirect('home')
+    else:
+        form = BillboardForm()
+    return render(request, 'create_billboard.html', {'form': form})
